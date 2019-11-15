b89130dcfbc93ea5391d4b1ffc0d22dc5780d254
aurelie976 <aurelie.nourdine@outlook.fr>
2019-11-15 11:04:12 +0000

    ajout js joueur vs machine

--- js/main.js
+++ js/main.js
@@ -191,3 +191,772 @@
 //   // On initialise le plateau et on visualise dans le DOM
 //   // (dans la balise d'identifiant `game`).
 //   let p4 = new Puissance4('#game');
+
+var grille = new Array();
+		var partiefinie = false;
+		var nbCoups = 0;
+		
+		for (i=0; i < 7; i++)
+		{
+			grille[i] = new Array();
+		}
+		
+		for (i=0; i < 7; i++)
+		{
+			for (j=0; j < 6; j++)
+			{
+				grille[i][j] = 0;
+			}
+		}
+		
+		function rejouer()
+		{
+			for (i=1; i <= 7; i++)
+			{
+				for (j=1; j <= 6; j++)
+				{
+					grille[i-1][j-1] = 0;
+					document.getElementById(i + "" + j + "").innerHTML = "";
+				}
+			}
+			partiefinie = false;
+			nbCoups = 0;
+			document.getElementById("rejouer").innerHTML = "";
+			document.getElementById("message").innerHTML = "";
+		}
+		
+		function select(col)
+		{
+			if (!partiefinie)
+			{
+				for (i=1; i <= 6; i++)
+				{
+					document.getElementById(col + "" + i + "").style.opacity = '0.8';
+				}
+			}
+		}
+		
+		function unselect(col)
+		{
+			if (!partiefinie)
+			{
+				for (i=1; i <= 6; i++)
+				{
+					document.getElementById(col + "" + i + "").style.opacity = '1';
+				}
+			}
+		}
+		
+		function unselectall()
+		{
+			if (!partiefinie)
+			{
+				for (j = 1; j <= 7; j++)
+				{
+					for (i=1; i <= 6; i++)
+					{
+						document.getElementById(j + "" + i + "").style.opacity = '1';
+					}
+				}
+				
+			}
+		}
+		
+		function gagner(joueurourobot, col, ligne)
+		{
+			var sommeG, sommeD;
+			
+			if (joueurourobot == "joueur")
+			{
+				id = 1;
+			}
+			else
+			{
+				id = 2;
+			}
+			
+			//Ligne
+			sommeG = 0;
+			sommeD = 0;
+			
+			for (i=col-1; i>=0 && grille[i][ligne] == id; i--)
+			{
+				sommeG++;
+			}
+			for (i=col+1; i<7 && grille[i][ligne] == id; i++)
+			{
+				sommeD++;
+			}
+			
+			if (sommeG+sommeD+1 >= 4)
+			{
+				unselectall();
+				partiefinie = true;
+				if (id == 1)
+				{
+					document.getElementById("message").innerHTML = "Vous avez gagné !";
+				}
+				else
+				{
+					document.getElementById("message").innerHTML = "Vous avez perdu !";
+				}
+			}
+			
+			//Colonne
+			if (!partiefinie)
+			{
+				sommeB = 0;
+				
+				for (i=ligne; i < 6 && grille[col][i] == id; i++)
+				{
+					sommeB++;
+				}
+				
+				if (sommeB == 4)
+				{
+					unselectall();
+					partiefinie = true;
+					if (id == 1)
+					{
+						document.getElementById("message").innerHTML = "Vous avez gagné !";
+					}
+					else
+					{
+						document.getElementById("message").innerHTML = "Vous avez perdu !";
+					}
+					
+				}
+				
+				//Diagonale haut gauche, droite bas
+				if (!partiefinie)
+				{
+					sommeG = 0;
+					sommeD = 0;
+					
+					i = col-1;
+					j = ligne-1;
+					
+					while (i >= 0 && j >= 0 && grille[i][j] == id)
+					{
+						sommeG++;
+						i--;
+						j--;
+					}
+					
+					i = col+1;
+					j = ligne+1;
+					
+					while (i < 7 && j < 6 && grille[i][j] == id)
+					{
+						sommeD++;
+						i++;
+						j++;
+					}
+					
+					if (sommeD + sommeG + 1 >= 4)
+					{
+						unselectall();
+						partiefinie = true;
+						if (id == 1)
+						{
+							document.getElementById("message").innerHTML = "Vous avez gagné !";
+						}
+						else
+						{
+							document.getElementById("message").innerHTML = "Vous avez perdu !";
+						}
+					}
+					
+					if (!partiefinie)
+					{
+						sommeG = 0;
+						sommeD = 0;
+						
+						i = col-1;
+						j = ligne+1;
+						
+						while (i >= 0 && j < 6 && grille[i][j] == id)
+						{
+							sommeG++;
+							i--;
+							j++;
+						}
+						
+						i = col+1;
+						j = ligne-1;
+						
+						while (i < 7 && j >= 0 && grille[i][j] == id)
+						{
+							sommeD++;
+							i++;
+							j--;
+						}
+						
+						if (sommeD + sommeG + 1 >= 4)
+						{
+							unselectall();
+							partiefinie = true;
+							if (id == 1)
+							{
+								document.getElementById("message").innerHTML = "Vous avez gagné !";
+							}
+							else
+							{
+								document.getElementById("message").innerHTML = "Vous avez perdu !";
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		function possibiliteDeGagnerOuBloquer()
+		{
+			res = false;
+			
+			for (c=0; c < 7 && !res; c++)//On cherche si on peut gagner
+			{
+				for (l=0; l < 6 && !res; l++)
+				{
+					if ((l == 5 && grille[c][l] == 0) || (l < 5 && grille[c][l] == 0 && grille[c][l+1] != 0))
+					{
+						//Ligne
+						sommeG = 0;
+						sommeD = 0;
+						
+						for (i=c-1; i>=0 && grille[i][l] == 2; i--)
+						{
+							sommeG++;
+						}
+						for (i=c+1; i<7 && grille[i][l] == 2; i++)
+						{
+							sommeD++;
+						}
+						
+						if (sommeG+sommeD >= 3)
+						{
+							ligne = l;
+							colonne = c;
+							res = true;
+						}
+						
+						//Colonne
+						if (!res)
+						{
+							sommeB = 0;
+							
+							for (i=l+1; i < 6 && grille[c][i] == 2; i++)
+							{
+								sommeB++;
+							}
+							
+							if (sommeB == 3)
+							{
+								ligne = l;
+								colonne = c;
+								res = true;
+							}
+							
+							//Diagonale
+							if (!res)
+							{
+								sommeG = 0;
+								sommeD = 0;
+								
+								i = c-1;
+								j = l-1;
+								
+								while (i >= 0 && j >= 0 && grille[i][j] == 2)
+								{
+									sommeG++;
+									i--;
+									j--;
+								}
+								
+								i = c+1;
+								j = l+1;
+								
+								while (i < 7 && j < 6 && grille[i][j] == 2)
+								{
+									sommeD++;
+									i++;
+									j++;
+								}
+								
+								if (sommeD + sommeG >= 3)
+								{
+									ligne = l;
+									colonne = c;
+									res = true;
+								}
+								
+								if (!res)
+								{
+									sommeG = 0;
+									sommeD = 0;
+									
+									i = c-1;
+									j = l+1;
+									
+									while (i >= 0 && j < 6 && grille[i][j] == 2)
+									{
+										sommeG++;
+										i--;
+										j++;
+									}
+									
+									i = c+1;
+									j = l-1;
+									
+									while (i < 7 && j >= 0 && grille[i][j] == 2)
+									{
+										sommeD++;
+										i++;
+										j--;
+									}
+									
+									if (sommeD + sommeG >= 3)
+									{
+										ligne = l;
+										colonne = c;
+										res = true;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			for (c=0; c < 7 && !res; c++)//On cherche si on peut bloquer
+			{
+				for (l=0; l < 6 && !res; l++)
+				{
+					if ((l == 5 && grille[c][l] == 0) || (l < 5 && grille[c][l] == 0 && grille[c][l+1] != 0))
+					{
+						//Ligne
+						sommeG = 0;
+						sommeD = 0;
+						
+						for (i=c-1; i>=0 && grille[i][l] == 1; i--)
+						{
+							sommeG++;
+						}
+						for (i=c+1; i<7 && grille[i][l] == 1; i++)
+						{
+							sommeD++;
+						}
+						
+						if (sommeG+sommeD >= 3)
+						{
+							ligne = l;
+							colonne = c;
+							res = true;
+						}
+						
+						//Colonne
+						if (!res)
+						{
+							sommeB = 0;
+							
+							for (i=l+1; i < 6 && grille[c][i] == 1; i++)
+							{
+								sommeB++;
+							}
+							
+							if (sommeB == 3)
+							{
+								ligne = l;
+								colonne = c;
+								res = true;
+							}
+							
+							//Diagonale
+							if (!res)
+							{
+								sommeG = 0;
+								sommeD = 0;
+								
+								i = c-1;
+								j = l-1;
+								
+								while (i >= 0 && j >= 0 && grille[i][j] == 1)
+								{
+									sommeG++;
+									i--;
+									j--;
+								}
+								
+								i = c+1;
+								j = l+1;
+								
+								while (i < 7 && j < 6 && grille[i][j] == 1)
+								{
+									sommeD++;
+									i++;
+									j++;
+								}
+								
+								if (sommeD + sommeG >= 3)
+								{
+									ligne = l;
+									colonne = c;
+									res = true;
+								}
+								
+								if (!res)
+								{
+									sommeG = 0;
+									sommeD = 0;
+									
+									i = c-1;
+									j = l+1;
+									
+									while (i >= 0 && j < 6 && grille[i][j] == 1)
+									{
+										sommeG++;
+										i--;
+										j++;
+									}
+									
+									i = c+1;
+									j = l-1;
+									
+									while (i < 7 && j >= 0 && grille[i][j] == 1)
+									{
+										sommeD++;
+										i++;
+										j--;
+									}
+									
+									if (sommeD + sommeG >= 3)
+									{
+										ligne = l;
+										colonne = c;
+										res = true;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			return res;
+		}
+		
+		function strategie5()
+		{
+			res = false;
+			
+			for (c = 0; c < 3 && !res; c++)
+			{
+				for (l = 0; l < 5 && !res; l++)
+				{
+					if (grille[c][l] == 0 && grille[c+1][l] == 1 && grille[c+2][l] == 1 && grille[c+3][l] == 0 && grille[c+4][l] == 0 && grille[c+3][l] != 0)
+					{
+						res = true;
+						ligne = l;
+						colonne = c+3;
+					}
+				}
+				
+				if (!res && grille[c][5] == 0 && grille[c+1][5] == 1 && grille[c+2][5] == 1 && grille[c+3][5] == 0 && grille[c+4][5] == 0)
+				{
+					res = true;
+					ligne = 5;
+					colonne = c+3;
+				}
+			}
+			
+			
+			
+			for (c = 6; c > 3 && !res; c--)
+			{
+				for (l = 0; l < 5 && !res; l++)
+				{
+					if (grille[c][l] == 0 && grille[c-1][l] == 1 && grille[c-2][l] == 1 && grille[c-3][l] == 0 && grille[c-4][l] == 0 && grille[c-3][l] != 0)
+					{
+						res = true;
+						ligne = l;
+						colonne = c-3;
+					}
+				}
+				
+				if (!res && grille[c][5] == 0 && grille[c-1][5] == 1 && grille[c-2][5] == 1 && grille[c-3][5] == 0 && grille[c-4][5] == 0)
+				{
+					res = true;
+					ligne = 5;
+					colonne = c-3;
+				}
+			}
+			
+			for (c = 0; c < 3 && !res; c++)
+			{
+				for (l = 0; l < 5 && !res; l++)
+				{
+					if (grille[c][l] == 0 && grille[c+1][l] == 2 && grille[c+2][l] == 2 && grille[c+3][l] == 0 && grille[c+4][l] == 0 && grille[c+3][l] != 0)
+					{
+						res = true;
+						ligne = l;
+						colonne = c+3;
+					}
+				}
+				
+				if (!res && grille[c][5] == 0 && grille[c+1][5] == 2 && grille[c+2][5] == 2 && grille[c+3][5] == 0 && grille[c+4][5] == 0)
+				{
+					res = true;
+					ligne = 5;
+					colonne = c+3;
+				}
+			}
+			
+			for (c = 6; c > 3 && !res; c--)
+			{
+				for (l = 0; l < 5 && !res; l++)
+				{
+					if (grille[c][l] == 0 && grille[c-1][l] == 2 && grille[c-2][l] == 2 && grille[c-3][l] == 0 && grille[c-4][l] == 0 && grille[c-3][l] != 0)
+					{
+						res = true;
+						ligne = l;
+						colonne = c-3;
+					}
+				}
+				
+				if (!res && grille[c][5] == 0 && grille[c-1][5] == 2 && grille[c-2][5] == 2 && grille[c-3][5] == 0 && grille[c-4][5] == 0)
+				{
+					res = true;
+					ligne = 5;
+					colonne = c-3;
+				}
+			}
+			
+			return res;
+		}
+		
+		function presqueGagneAdversaire(l, c)
+		{
+			res = false;
+			
+			//Ligne
+			sommeG = 0;
+			sommeD = 0;
+			
+			for (i=c-1; i>=0 && grille[i][l] == 1; i--)
+			{
+				sommeG++;
+			}
+			for (i=c+1; i<7 && grille[i][l] == 1; i++)
+			{
+				sommeD++;
+			}
+			
+			if (sommeG+sommeD >= 3)
+			{
+				res = true;
+			}
+			
+			//Colonne
+			if (!res)
+			{
+				sommeB = 0;
+				
+				for (i=l+1; i < 6 && grille[c][i] == 1; i++)
+				{
+					sommeB++;
+				}
+				
+				if (sommeB == 3)
+				{
+					res = true;
+				}
+				
+				//Diagonale
+				if (!res)
+				{
+					sommeG = 0;
+					sommeD = 0;
+					
+					i = c-1;
+					j = l-1;
+					
+					while (i >= 0 && j >= 0 && grille[i][j] == 1)
+					{
+						sommeG++;
+						i--;
+						j--;
+					}
+					
+					i = c+1;
+					j = l+1;
+					
+					while (i < 7 && j < 6 && grille[i][j] == 2)
+					{
+						sommeD++;
+						i++;
+						j++;
+					}
+					
+					if (sommeD + sommeG >= 3)
+					{
+						res = true;
+					}
+					
+					if (!res)
+					{
+						sommeG = 0;
+						sommeD = 0;
+						
+						i = c-1;
+						j = l+1;
+						
+						while (i >= 0 && j < 6 && grille[i][j] == 2)
+						{
+							sommeG++;
+							i--;
+							j++;
+						}
+						
+						i = c+1;
+						j = l-1;
+						
+						while (i < 7 && j >= 0 && grille[i][j] == 2)
+						{
+							sommeD++;
+							i++;
+							j--;
+						}
+						
+						if (sommeD + sommeG >= 3)
+						{
+							res = true;
+						}
+					}
+				}
+			}
+			
+			return res;
+		}
+		
+		function nbColonnesVides()
+		{
+			var nb = 0;
+			
+			for (i = 0; i < 7; i++)
+			{
+				if (grille[i][0] == 0)
+				{
+					nb++;
+				}
+			}
+			
+			return nb;
+		}
+		
+		function jeu(col)
+		{
+			if (grille[col-1][0] == 0 && !partiefinie)//On joue seulement si la colonne n'est pas pleine
+			{
+				//Tour du joueur---------------------------------------------
+				
+				ligne = 0;
+				while (grille[col-1][ligne] == 0)
+				{
+					ligne++;
+				}//Recherche de la première case vide où on devra mettre le jeton
+				
+				document.getElementById(col + "" + ligne + "").innerHTML = '<img src="rouge.png" alt="rouge"/>';
+				grille[col-1][ligne-1] = 1;
+				
+				gagner("joueur", col-1, ligne-1);
+				
+				
+				//Tour de l'IA-----------------------------------------------
+				
+				if (!partiefinie)
+				{
+					colonne = -1;
+					ligne = -1;
+					
+					if (possibiliteDeGagnerOuBloquer())
+					{
+						colonne = colonne + 1;
+						ligne = ligne + 1;
+						
+						document.getElementById(colonne + "" + ligne +"").innerHTML = '<img src="jaune.png" alt="jaune"/>';
+						grille[colonne-1][ligne-1] = 2;
+						
+						gagner("adversaire", colonne-1, ligne-1);
+					}
+					else
+					{
+					
+						colonne = -1;
+						ligne = -1;
+						
+						if (strategie5())
+						{
+							colonne = colonne + 1;
+							ligne = ligne + 1;
+							
+							document.getElementById(colonne + "" + ligne +"").innerHTML = '<img src="jaune.png" alt="jaune"/>';
+							grille[colonne-1][ligne-1] = 2;
+							
+							gagner("adversaire", colonne-1, ligne-1);
+						}
+						else
+						{
+							
+							if (nbColonnesVides() > 1)
+							{
+								do
+								{
+									do
+									{
+										colonne = Math.floor(Math.random()*7);
+									}while(grille[colonne][0] != 0);
+									
+									ligne = 0;
+									while (grille[colonne][ligne] == 0)
+									{
+										ligne++;
+									}//Recherche de la première case vide où on devra mettre le jeton
+								}while (presqueGagneAdversaire(ligne-2, colonne));
+							}
+							else
+							{
+								do
+								{
+									colonne = Math.floor(Math.random()*7);
+								}while(grille[colonne][0] != 0);
+								
+								ligne = 0;
+								while (grille[colonne][ligne] == 0)
+								{
+									ligne++;
+								}//Recherche de la première case vide où on devra mettre le jeton
+							}
+							
+							document.getElementById(colonne + 1 + "" + ligne + "").innerHTML = '<img src="jaune.png" alt="jaune"/>';
+							grille[colonne][ligne-1] = 2;
+							
+							gagner("adversaire", colonne, ligne-1);
+						}
+						
+					}
+					
+					nbCoups += 2;
+					if (nbCoups >= 42)
+					{
+						unselectall();
+						partiefinie = true;
+						document.getElementById("message").innerHTML = "Egalité !";
+					}
+				}
+				
+				if (partiefinie)
+				{
+					document.getElementById("rejouer").innerHTML = '<input type="button" value="Rejouer?" onclick="rejouer()" />';
+				}
+			}
+		}
