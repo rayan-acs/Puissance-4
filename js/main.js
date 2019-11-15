cea9a6afdfa8eff6941f7cf0942931f7774c56af
ericc70 <eric.c@codeur.online>
2019-11-15 13:50:04 +0000

    premier idée jeux

---
+++ js/main.js
@@ -0,0 +1,118 @@
+// definition de la grille en js
+
+var grille = new Array(7);
+
+for (var i = 0; i < 6; i++) {
+    grille[i] = new Array(7);
+}
+// rempli le tableau de "vide"
+for (var i = 0; i < 6; i++) {
+    for (var j = 0; j < 7; j++) {
+        grille[i][j] = "vide";
+    }
+}
+var joueurActif;
+
+var clickedCellCoordX;
+var game = document.querySelector('.grille-jeux');
+
+game.addEventListener('click', function (event) {
+    console.log(grille)
+    // clickedCellElmt = document.querySelector(event.target.id)
+    clickedCellElmt = document.getElementById(event.target.id);
+    clickedCellName = event.target.id;
+    console.log(clickedCellName);
+    clickedCellCoordY = parseInt(clickedCellName[2]); // 2nd param id
+    console.log(clickedCellCoordY);
+
+
+    pion();
+    color();
+    changementJoueur(); 
+    afficherJoueurActif();
+
+
+    victoireColonne()
+})
+
+
+//  fonction appliquer valeur joueur
+
+function color() {
+    var cc = '#c' + clickedCellCoordX + clickedCellCoordY;
+    console.log(cc)
+    if (joueurActif == 1) {
+        document.querySelector(cc).classList.add("jaune");
+
+        grille[clickedCellCoordX][clickedCellCoordY] = "jaune";
+        console.log('grille[' + clickedCellCoordX + '][' + clickedCellCoordY + ']');
+    } else if (joueurActif == 2) {
+        document.querySelector(cc).classList.add("rouge");
+
+        grille[clickedCellCoordX][clickedCellCoordY] = "rouge";
+        console.log('grille[' + clickedCellCoordX + '][' + clickedCellCoordY + ']');
+    }
+}
+//fonction pion 
+
+function pion() {
+
+    if (grille[5][clickedCellCoordY] == "vide") {
+        clickedCellCoordX = 5;
+
+    } else if (grille[4][clickedCellCoordY] == "vide") {
+        clickedCellCoordX = 4;
+    } else if (grille[3][clickedCellCoordY] == "vide") {
+        clickedCellCoordX = 3;
+    } else if (grille[2][clickedCellCoordY] == "vide") {
+        clickedCellCoordX = 2;
+    } else if (grille[1][clickedCellCoordY] == "vide") {
+        clickedCellCoordX = 1;
+    } else if (grille[0][clickedCellCoordY] == "vide") {
+        clickedCellCoordX = 0;
+    }
+
+    console.log("clickedCellCoordX " + clickedCellCoordX)
+    console.log("clickedCellCoordY " + clickedCellCoordY)
+}
+
+function afficherJoueurActif() {
+
+    // if (win == false) {
+    if (joueurActif == 1) {
+        document.querySelector(".joueur1").style.color = "yellow";
+        document.querySelector(".joueur2").style.color = "black";
+
+    } else if (joueurActif == 2) {
+        document.querySelector(".joueur1").style.color = "black";
+        document.querySelector(".joueur2").style.color = "red";
+    } else {
+        alert('le pc va exploser');
+    }
+    //  }
+}
+
+// fonction pour changement de joueur 
+
+var joueur = true;
+var joueurActif = 0;
+
+
+function changementJoueur() {
+    if (joueur == true) {
+        joueurActif = 1;
+        joueur = false;
+    } else {
+        joueurActif = 2;
+        joueur = true;
+    }
+}
+
+
+
+// function gagner()
+//     {
+// console.log('winnnner')
+
+// }
+
